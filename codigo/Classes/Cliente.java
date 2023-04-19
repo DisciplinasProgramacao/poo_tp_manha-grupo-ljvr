6df28822fd5e3e3a86e51e02bcb6fdf464d14c5f
rlrosaa <79370543+rlrosaa@users.noreply.github.com>
2023-04-18 22:20:12 -0300

    Melhoria na documentação.

--- codigo/Classes/Cliente.java
+++ codigo/Classes/Cliente.java
@@ -48,15 +48,26 @@ public class Cliente{
         Random nRandom = new Random(1);
         return Integer.toString(nRandom.nextInt(10000000));
     }
-
+    
+    /**
+     * Adiciona uma série à lista de Series para Assistir Futuramente de um cliente especifico.
+     * @param serie
+     */
     public void adicionarListaSeriesFuturas(Serie serie){
         listaSeriesFuturas.add(serie);
     }
 
+    /**
+     * Remove uma série especifica da lista de Series para Assistir Futuramente de um cliente.
+     * @param serie
+     */
     public void removerListaSeriesFuturas(Serie serie){
         listaSeriesFuturas.remove(serie);
     }
 
+    /**
+     * Adiciona uma série à lista de Series Assistidas de um cliente especifico.
+     */
     public void adicionarListaSeriesAssistidas (Serie serie){
         listaSeriesAssistidas.add(serie);
     }
