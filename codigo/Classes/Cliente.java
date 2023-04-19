ad78d939818d9d5a6d41294c6ede7d3d5c1b4bab
rlrosaa <79370543+rlrosaa@users.noreply.github.com>
2023-04-19 15:31:43 -0300

    Criação de metodos GET na classe Cliente. Melhorias de código

--- codigo/Classes/Cliente.java
+++ codigo/Classes/Cliente.java
@@ -1,44 +1,48 @@
 package Classes;
+
 import java.util.HashSet;
 import java.util.Random;
 
-public class Cliente{
+public class Cliente {
 
-    private String email,senha,nome;
+    private String email;
+    private String senha;
+    private String nome;
     private HashSet<Serie> listaSeriesFuturas = new HashSet<>();
     private HashSet<Serie> listaSeriesAssistidas = new HashSet<>();
 
     /**
      * Cria o Objeto Cliente recebendo os dados do usuario.
-     * Se o dado nome receber qualquer valor invalido ou em branco, será mantido em branco.
-     * Se os dados e-mail ou senha forem inseridos invalidos, seraão criados aleatorios para o cliente.
+     * Se o dado nome receber qualquer valor invalido ou em branco, será mantido em
+     * branco.
+     * Se os dados e-mail ou senha forem inseridos invalidos, seraão criados
+     * aleatorios para o cliente.
+     * 
      * @param nome
      * @param email
      * @param senha
      */
-    public Cliente (String nome, String email, String senha){
-        inicializar (nome,email,senha);
+    public Cliente(String nome, String email, String senha) {
+        inicializar(nome, email, senha);
     }
 
-    private void inicializar (String nome, String email, String senha){
-        
-        if(email.isEmpty() || !email.contains("@")){
+    private void inicializar(String nome, String email, String senha) {
+
+        if (email.isEmpty() || !email.contains("@")) {
             this.email = gerarEmail();
-        }
-        else{
+        } else {
             this.email = email;
         }
 
-        if(senha.isEmpty()){
-          this.senha = gerarSenha();  
-        }
-        else{
+        if (senha.isEmpty()) {
+            this.senha = gerarSenha();
+        } else {
             this.senha = senha;
         }
 
         this.nome = nome;
     }
-    
+
     private String gerarEmail() {
         Random nRandom = new Random(1);
         return (Integer.toString(nRandom.nextInt(10000000)) + "@plataforma.com");
@@ -48,52 +52,64 @@ public class Cliente{
         Random nRandom = new Random(1);
         return Integer.toString(nRandom.nextInt(10000000));
     }
-    
+
     /**
-     * Adiciona uma série à lista de Series para Assistir Futuramente de um cliente especifico.
+     * Adiciona uma série à lista de Series para Assistir Futuramente de um cliente
+     * especifico.
+     * 
      * @param serie
      */
-    public void adicionarListaSeriesFuturas(Serie serie){
+    public void adicionarListaSeriesFuturas(Serie serie) {
         listaSeriesFuturas.add(serie);
     }
 
     /**
-     * Remove uma série especifica da lista de Series para Assistir Futuramente de um cliente.
+     * Remove uma série especifica da lista de Series para Assistir Futuramente de
+     * um cliente.
+     * 
      * @param serie
      */
-    public void removerListaSeriesFuturas(Serie serie){
+    public void removerListaSeriesFuturas(Serie serie) {
         listaSeriesFuturas.remove(serie);
     }
 
     /**
      * Adiciona uma série à lista de Series Assistidas de um cliente especifico.
      */
-    public void adicionarListaSeriesAssistidas (Serie serie){
+    public void adicionarListaSeriesAssistidas(Serie serie) {
         listaSeriesAssistidas.add(serie);
     }
 
-    public void filtrarSeriesFuturasNome (String nome){
-
+    public String getEmail() {
+        return this.email;
     }
 
-    public void filtrarSeriesFuturasIdioma (String idioma){
-        
+    public int getSenha() {
+        return this.senha.hashCode();
     }
 
-    public void filtrarSeriesFuturasGenero (String genero){
-        
+    public void filtrarSeriesFuturasNome(String nome) {
+        // TO DO
     }
 
-    public void filtrarSeriesAssistidassNome (String nome){
-
+    public void filtrarSeriesFuturasIdioma(String idioma) {
+        // TO DO
     }
 
-    public void filtrarSeriesAssistidassIdioma (String idioma){
-        
+    public void filtrarSeriesFuturasGenero(String genero) {
+        // TO DO
     }
 
-    public void filtrarSeriesAssistidassGenero (String genero){
-        
+    public void filtrarSeriesAssistidassNome(String nome) {
+        // TO DO
+    }
+
+    public void filtrarSeriesAssistidassIdioma(String idioma) {
+        // TO DO
+    }
+
+    public void filtrarSeriesAssistidassGenero(String genero) {
+        // TO DO
     }
 
 }
