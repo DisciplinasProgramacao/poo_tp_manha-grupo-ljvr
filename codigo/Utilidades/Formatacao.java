de0315c498894e75d1bd18a9d417f2e8e8297c74
Luiz Henrique Oliveira de Assis <51269270+luizhenrique055@users.noreply.github.com>
2023-05-02 13:05:09 -0300

    Melhoria doc

--- codigo/Utilidades/Formatacao.java
+++ codigo/Utilidades/Formatacao.java
@@ -2,19 +2,21 @@ import java.util.StringTokenizer;
 
 class Formatacao {
 
-    // possivel implementacao de realocacao de capacidade automatica caso necessario
+    // possivel implementacao de realocacao de capacidade automatica e troca de
+    // delimitador caso necessario
 
     /**
-     * Recebe uma linha(String) contendo delimitadores (;) e os retorna em um unico
-     * array
-     * separado pelos delimitadores
+     * Pega uma linha de arquivo String com até 5 delimitadores ( ; )
+     * partindo a linha e a transformando em um unico Array.
+     * Caso a a linha do arquivo seja menor que 5 delimitadores as unidades
+     * sem preenchimento serão null.
      * 
-     * @param linha String "Delim1;Delim2;Delim3"
-     * @return String Array[3]
+     * @param linha String "Delim1;Delim2..."
+     * @return String[5]
      */
     public static String[] separadorPtVirgula(String linha) {
 
-        String[] array = new String[3];
+        String[] array = new String[5];
         int posicao = 0;
 
         StringTokenizer st = new StringTokenizer(linha, ";");
