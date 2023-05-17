a3a089567316636b6921c216eaed74986cab02f9
Luiz Henrique Oliveira de Assis <51269270+luizhenrique055@users.noreply.github.com>
2023-05-17 19:59:30 -0300

    Criação classe FilmeLonga

---
+++ codigo/Classes/FilmeLonga.java
@@ -0,0 +1,39 @@
+package Classes;
+
+import java.time.LocalDate;
+
+public class FilmeLonga extends Midia {
+
+    // atributos
+    private int duracaoMin;
+
+    /**
+     * Construtor de FilmeLonga.
+     * <p>
+     * Caso duracaoMin for menor que 1 ira disparar uma IllegalArgumentException
+     * 
+     * @param idMidia        int
+     * @param nome           String
+     * @param idioma         String
+     * @param genero         String
+     * @param dataLancamento LocalDate
+     * @param duracaoMin     String
+     */
+    public FilmeLonga(int idMidia, String nome, String idioma, String genero, LocalDate dataLancamento,
+            int duracaoMin) {
+
+        super(idMidia, nome, idioma, genero, dataLancamento);
+
+        if (duracaoMin > 0)
+            this.duracaoMin = duracaoMin;
+        else
+            throw new IllegalArgumentException("A duracao do filme nao pode ser menor que 1");
+
+    }
+
+    // get
+    public int getDuracaoMin() {
+        return duracaoMin;
+    }
+
+}
