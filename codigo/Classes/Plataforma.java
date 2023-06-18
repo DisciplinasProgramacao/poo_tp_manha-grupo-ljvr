230e66b3461b82ee780fd7e37c26b964f145863a
Luiz Henrique Oliveira de Assis <51269270+luizhenrique055@users.noreply.github.com>
2023-06-18 11:52:53 -0300

    Plataforma v2
    Nova classe plataforma com os requisitos modificados.
    
    Remoção de alguns métodos
    Remoção de sets
    Modificação de nomes

--- codigo/Classes/Plataforma.java
+++ codigo/Classes/Plataforma.java
@@ -2,55 +2,78 @@ package Classes;
 
 import java.util.HashMap;
 
+// TO DO: rever funcionamento da classe antes da implementação final e remover code smell antes de publicar o app
+
 public class Plataforma {
 
 	// atributos
-	private HashMap<String,Serie> listSeriesGeral;
+	private HashMap<String, Midia> tableMidiasGerais;
 	private HashMap<String, Cliente> tableClientes;
 	private Cliente usuarioAtual;
 
 	/**
 	 * O construtor da classe Pataforma
 	 * 
-	 * @param listSeriesGeral ArrayList que recebe como parametro os dados da classe Serie
-	 *                       
-	 * @param tableClientes   Hashtable recebe como parametros os dados do cliente da Classe Cliente
-	 *                     
-	 * @param usuarioAtual    
-	 * @return
+	 * @param tableMidiasGerais HashMap que recebe como parametros os dados da
+	 *                          classe Midia, com a chave sendo uma String
+	 * 
+	 * @param tableClientes     Hashtable que recebe como parametros os dados da
+	 *                          da classe Cliente, com a chave sendo uma String
+	 * 
+	 * @param usuarioAtual      Usuario atual sendo manuseado pela plataforma
+	 * 
 	 */
 	public Plataforma() {
-		listSeriesGeral = new HashMap<String,Serie>();
+		tableMidiasGerais = new HashMap<String, Midia>();
 		tableClientes = new HashMap<String, Cliente>();
 		usuarioAtual = null;
 	}
 
-	// Metodos de acesso e controle
-	
+	// metodo de login
+	// TO DO: rever funcionamento do metodo fazerLogin
+
+	/**
+	 * O metodo fazerLogin recebe como parametros o e-mail e a senha do cliente e
+	 * realiza a validacao do login. Caso o e-mail esteja na tabela hash
+	 * tableClientes e a senha seja valida, o metodo define o cliente
+	 * correspondente
+	 * como usuarioAtual e retorna true. Caso contrario, o metodo retorna false
+	 *
+	 * @param email
+	 * @param senha
+	 * @return
+	 */
+	public boolean fazerLogin(String email, String senha) {
+		if (tableClientes.containsKey(email)) {
+			Cliente cliente = tableClientes.get(email);
+			if (cliente.getSenha() == senha.hashCode()) {
+				this.usuarioAtual = cliente;
+				return true;
+			}
+		}
+		return false;
+	}
+
+	// metodos de acesso
+
+	/**
+	 * Retorna uma Midia, bunscando o partir do ID.
+	 * 
+	 * @param id ID da Midia.
+	 * @return Midia.
+	 */
+	public Midia encontrarMidia(String id) {
+		return tableMidiasGerais.get(id);
+	}
+
 	/**
 	 * Retorna um Cliente, bunscando o partir do Login.
+	 * 
 	 * @param login Login do Usuario.
 	 * @return Cliente.
 	 */
 	public Cliente encontrarCliente(String login) {
 		return tableClientes.get(login);
-	}
-
-	/**
-	 * Retorna uma Midia, bunscando o partir do ID.
-	 * @param id ID da Midia.
-	 * @return Midia.
-	 */
-	public Serie encontrarSerie(String id) {
-		return listSeriesGeral.get(id);
-	}
-
-	public Cliente getUsuarioAtual() {
-		return usuarioAtual;
-	}
-
-	private void setUsuarioAtual(Cliente usuarioAtual) {
-		this.usuarioAtual = usuarioAtual;
 	}
 
 	/**
@@ -60,80 +83,16 @@ public class Plataforma {
 	 * @param novoCliente
 	 */
 	public void adicionarCliente(Cliente novoCliente) {
-		tableClientes.put(novoCliente.getLogin(), novoCliente); 
+		tableClientes.put(novoCliente.getLogin(), novoCliente);
 	}
 
 	/**
-	 * O metodo removerCliente remove um cliente da tabela hashMap, utilizando o login
-	 * como chave
+	 * Adiciona uma nova midia em tableMidiaGerais
 	 * 
-	 * @param email
+	 * @param novaMidia
 	 */
-	public void removerCliente(String login) {
-		tableClientes.remove(login);
+	public void adicionarMidia(String id, Midia novaMidia) {
+		tableMidiasGerais.put(id, novaMidia);
 	}
 
-	/**
-	 * O metodo adicionarSerie adiciona uma nova serie no ArrayList listSeriesGeral
-	 * 
-	 * @param novaSerie
-	 */
-	public void adicionarSerie(String id , Serie novaSerie) {
-		listSeriesGeral.put(id, novaSerie);
-	}
-
-	/**
-	 * O metodo removerSerie remove uma serie do ArrayList listSeriesGeral
-	 * 
-	 * @param serie
-	 */
-	public void removerSerie(String id) {
-		listSeriesGeral.remove(id);
-	}
-
-	/**
-	 * O metodo fazerLogin recebe como parametros o e-mail e a senha do cliente e
-	 * realiza a validacao do login. Caso o e-mail esteja na tabela hash
-	 * tableClientes e a senha seja valida, o metodo define o cliente correspondente
-	 * como usuarioAtual e retorna true. Caso contrario, o metodo retorna false
-	 * 
-	 * @param email
-	 * @param senha
-	 * @return
-	 */
-	public boolean fazerLogin(String email, String senha) {
-		if (tableClientes.containsKey(email)) {
-			Cliente cliente = tableClientes.get(email);
-			if (cliente.getSenha() == senha.hashCode()) { 
-				setUsuarioAtual(cliente);
-				return true;
-			}
-		}
-		return false;
-	}
-	
-	// a fazer
-
-	/**
-	 * Metodo filtrarListaSerieGeralNome que recebe como parametro o nome da serie digitada pelo usuario 
-	 * @author jvasc
-	 */
-
-	public void filtrarListaSerieGeralNome() {
-		
-	}
-	
-	/**
-	 * Metodo filtrarListaSerieGeralIdioma recebe como parametro o idioma digitado pelo usuario
-	 */
-	public void filtrarListaSerieGeralIdioma() {
-		
-	}
-	
-	/**
-	 * Metodo filtrarListaSerieGeralIdioma recebe como parametro o idioma digitado pelo usuario
-	 */
-	public void filtrarListaSerieGeralGenero() {
-		
-	}
 }
